554aa442abdd68e860caa175c1b5c79f30e42b65
yayheniy <zhenya091992@gmail.com>
2022-11-17 15:24:28 +0300

    repair vite via https

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+
 
 export default defineConfig({
     server: {    // <-- this object is added
-        port: 5174
+        port: 5174,
     },
     plugins: [
+        basicSsl(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
